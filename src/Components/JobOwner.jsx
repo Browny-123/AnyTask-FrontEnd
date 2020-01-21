19996d30a5c9cb89582e237dbf08e7f62b3f7404
Browny-123 <andybrown098@live.co.uk>
2020-01-21 21:47:53 +0100

    changed image

--- src/Components/JobOwner.jsx
+++ src/Components/JobOwner.jsx
@@ -3,14 +3,19 @@ import React from "react";
 export default function JobOwner({ jobOwner }) {
   return (
     <div className="job-divider">
-      <h2 className="job-poster">Job Poster</h2>
-      <img
-        src={jobOwner.profilePicture}
-        alt="Profile"
-        className="job-search-user-pic"
-      />
-      <p>{jobOwner.name}</p>
-      <p>Rating: 5 Stars</p>
+      {!jobOwner ? null : (
+        <>
+          {" "}
+          <h2 className="job-poster">Job Poster</h2>
+          <img
+            src={jobOwner.profilePicture}
+            alt="Profile"
+            className="job-search-user-pic"
+          />
+          <p>{jobOwner.name}</p>
+          <p>Rating: 5 Stars</p>
+        </>
+      )}
     </div>
   );
 }
