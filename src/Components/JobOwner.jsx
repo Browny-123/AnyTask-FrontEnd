d325d9db4fb3c51f5424fbe4966a41e2d39a8f72
Browny-123 <andybrown098@live.co.uk>
2020-01-21 21:51:48 +0100

    removed condition

--- src/Components/JobOwner.jsx
+++ src/Components/JobOwner.jsx
@@ -3,19 +3,14 @@ import React from "react";
 export default function JobOwner({ jobOwner }) {
   return (
     <div className="job-divider">
-      {!jobOwner ? null : (
-        <>
-          {" "}
-          <h2 className="job-poster">Job Poster</h2>
-          <img
-            src={jobOwner.profilePicture}
-            alt="Profile"
-            className="job-search-user-pic"
-          />
-          <p>{jobOwner.name}</p>
-          <p>Rating: 5 Stars</p>
-        </>
-      )}
+      <h2 className="job-poster">Job Poster</h2>
+      <img
+        src={jobOwner.profilePicture}
+        alt="Profile"
+        className="job-search-user-pic"
+      />
+      <p>{jobOwner.name}</p>
+      <p>Rating: 5 Stars</p>
     </div>
   );
 }
